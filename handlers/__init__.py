357173504791a37eb64667b09b7edb00a7077035
TinkiVinki <viper97@mail.ru>
2023-08-30 21:41:29 +0500

    Create template project

---
+++ handlers/__init__.py
@@ -0,0 +1,3 @@
+from handlers import commands
+from handlers import messages
+from handlers import callbacks
---
+++ loader.py
@@ -0,0 +1,13 @@
+from aiogram import Bot, Dispatcher
+from aiogram.utils.callback_data import CallbackData
+
+from config import bot_config
+from utils.set_bot_commands import set_bot_commands
+
+bot = Bot(token=bot_config.BOT_TOKEN)
+dp = Dispatcher(bot=bot)
+
+cb = CallbackData('ikb', 'action')
+
+async def on_startup(_):
+    await set_bot_commands(dp=dp)
